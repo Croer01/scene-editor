1ab7b804a97a97298232e3407e10c29094524fe0
Adria Rodriguez <adria_r_p_@hotmail.com>
2016-08-21 11:49:41 +0200

    cast "any" objects to avoid WebStorm throw errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 /**
  * Created by Adria on 20/08/2016.
  */
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {NgModuleMetadataType} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import {AppComponent}  from './app.component';
 
-@NgModule({
-    imports: [ BrowserModule ],
-    declarations: [ AppComponent ],
-    bootstrap: [ AppComponent ]
+@NgModule(<NgModuleMetadataType>{
+    imports: [BrowserModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,7 @@
 import 'zone.js/dist/zone';
 import 'reflect-metadata';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { ConcreteType } from '@angular/platform-browser-dynamic/src/facade/lang';
 import { AppModule } from './app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(<ConcreteType<AppModule>>AppModule);
