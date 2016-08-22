c5ae5161ecb28293c8387e88b84f7423d421db47
Adria Rodriguez <adria_r_p_@hotmail.com>
2016-08-22 19:13:07 +0200

    get scene object from scene file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
 /**
  * Created by Adria on 20/08/2016.
  */
-import { Component } from '@angular/core';
+import electron = require('electron');
+import {Component} from '@angular/core';
+const {ipcRenderer} = electron;
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `<div class="container-fluid">
+	<div class="row">
+		<main-content class="col-xs-9"></main-content>
+		<inspector class="col-xs-3"></inspector>
+	</div>
+</div>`
 })
-export class AppComponent { }
+export class AppComponent {
+    constructor() {
+        ipcRenderer.on('scene-load-success', (event, arg) => {
+            console.log(arg)
+        });
+    }
+}
--- src/menu.ts
+++ src/menu.ts
@@ -2,15 +2,42 @@
  * Created by Adria on 20/08/2016.
  */
 
+import electron = require('electron');
+import yaml = require('js-yaml');
+import fs = require('fs')
+
+const {dialog} = electron;
+
 export var menuTemplate = [
     {
         label: 'File',
         submenu: [{
             label: 'Load',
             accelerator: 'CmdOrCtrl+L',
-        },{
+            click: (menuItem, browserWindow, event) => {
+                let options : Electron.OpenDialogOptions = {
+                    properties: ['openFile'],
+                    filters: [
+                        {name: 'Scene file', extensions: ['scene']}
+                    ]
+                };
+                dialog.showOpenDialog(browserWindow, options, (dialogResults)=> {
+                    if (dialogResults !== undefined && dialogResults.length === 1) {
+                        let filename:string = dialogResults[0];
+
+                        try {
+                            let doc = yaml.safeLoad(fs.readFileSync(filename, 'utf8'));
+                            browserWindow.send('scene-load-success', doc);
+                        } catch (e) {
+                            browserWindow.send('scene-load-fail', e);
+                            console.log(e);
+                        }
+                    }
+                });
+            }
+        }, {
             label: 'Save',
-            accelerator: 'CmdOrCtrl+S',
+            accelerator: 'CmdOrCtrl+S'
         }]
     }
 ];
